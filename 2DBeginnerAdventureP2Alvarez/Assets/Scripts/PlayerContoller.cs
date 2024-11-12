7ff2be64a9818b7e625a503c1480db2bcbee71ec
JulianDUSDWHS <738743@student.dusd.net>
2024-11-12 10:00:52 -0800

    Progress
    added enemy

--- 2DBeginnerAdventureP2Alvarez/Assets/Scripts/PlayerContoller.cs
+++ 2DBeginnerAdventureP2Alvarez/Assets/Scripts/PlayerContoller.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerContoller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
     int currentHealth;
 
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -18,6 +22,7 @@ public class PlayerContoller : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+       
     }
 
     // Update is called once per frame
@@ -27,6 +32,14 @@ public class PlayerContoller : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0 )
+            {
+                isInvincible = false;
+            }
+        }
         
     }
     void FixedUpdate()
@@ -37,8 +50,17 @@ public class PlayerContoller : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
